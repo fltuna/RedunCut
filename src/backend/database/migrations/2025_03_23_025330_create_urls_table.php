694b1885054555811455389ad29989937431ea3b
faketuna <faketuna.st@gmail.com>
2025-03-24 02:15:27 +0900

    modify: mark user_id as nullable until auth feature implemented

--- src/backend/database/migrations/2025_03_23_025330_create_urls_table.php
+++ src/backend/database/migrations/2025_03_23_025330_create_urls_table.php
@@ -13,14 +13,16 @@ return new class extends Migration
     {
         Schema::create('urls', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained("users", "id");
+            //$table->foreignId('user_id')->constrained("users", "id");
+            // define user_id as nullable while authentication feature is not implemented.
+            $table->foreignId('user_id')->nullable();
             $table->string('original_url');
             $table->string('short_code')->unique();
             $table->string('custom_alias')->nullable();
             $table->timestamp('expires_at')->nullable();
             $table->integer('clicks')->default(0);
             $table->timestamps();
-            $table->softDeletes();  // 論理削除用
+            $table->softDeletes();  // Logical delete
         });
     }
 
