736466ec5baf8b86cb53290fd938e55dcebec2e5
faketuna <faketuna.st@gmail.com>
2025-03-24 02:17:25 +0900

    add: add phpdoc in Url model

--- src/backend/app/Models/Url.php
+++ src/backend/app/Models/Url.php
@@ -6,6 +6,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * @property int $id
+ * @property int $user_id
+ * @property string $original_url
+ * @property string $short_code
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ */
 class Url extends Model
 {
     use HasFactory, SoftDeletes;
