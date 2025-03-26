eb296102363174a77d9264332d5e1d8c59407813
faketuna <faketuna.st@gmail.com>
2025-03-26 11:59:22 +0900

    modify: define explicit return value in some funcs

--- src/backend/app/Http/Controllers/Api/V1/UrlController.php
+++ src/backend/app/Http/Controllers/Api/V1/UrlController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Http\Resources\UrlResource;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Validation\ValidationException;
 
 class UrlController extends Controller
@@ -19,7 +20,7 @@ class UrlController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): JsonResponse
     {
         $urls = Url::where('user_id', self::USER_ID_FOR_TEST_BEFORE_AUTH_IMPLEMENTED)
                     ->orderBy('created_at', 'desc')
@@ -31,7 +32,7 @@ class UrlController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): JsonResponse
     {
         try{
             $validated = $request->validate([
@@ -71,7 +72,7 @@ class UrlController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(string $id): JsonResponse
     {
         try {
             if(!ctype_digit($id))
@@ -94,7 +95,7 @@ class UrlController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, string $id): JsonResponse
     {
         try{
             $url = Url::find($id);
@@ -133,7 +134,7 @@ class UrlController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(string $id): JsonResponse
     {
         try{
             $url = Url::find($id);
