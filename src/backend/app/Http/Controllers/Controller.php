7c9cab4c23ce7c0e956c12607b422b3703e541cd
faketuna <faketuna.st@gmail.com>
2025-03-31 17:50:30 +0900

    modify: fix access modifier on Controller.php

--- src/backend/app/Http/Controllers/Controller.php
+++ src/backend/app/Http/Controllers/Controller.php
@@ -7,35 +7,35 @@ use Illuminate\Http\JsonResponse;
 abstract class Controller
 {
 
-    public function return404(string $message = 'Not Found'): JsonResponse
+    protected function return404(string $message = 'Not Found'): JsonResponse
     {
         return response()->json([
             'message' => $message
         ], 404);
     }
 
-    public function return500(string $message = 'Internal Server Error'): JsonResponse
+    protected function return500(string $message = 'Internal Server Error'): JsonResponse
     {
         return response()->json([
             'message' => $message
         ], 500);
     }
 
-    public function return422(string $message = 'Unprocessable Content'): JsonResponse
+    protected function return422(string $message = 'Unprocessable Content'): JsonResponse
     {
         return response()->json([
             'message' => $message
         ], 422);
     }
 
-    public function return400(string $message = 'Bad Request'): JsonResponse
+    protected function return400(string $message = 'Bad Request'): JsonResponse
     {
         return response()->json([
             'message' => $message,
         ], 400);
     }
 
-    public function return410(string $message = 'Gone'): JsonResponse
+    protected function return410(string $message = 'Gone'): JsonResponse
     {
         return response()->json([
             'message' => $message,
